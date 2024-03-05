4556036a0982f12b3bc8ea3e2ca84844335f85a3
Quang Anh <quanganhpham31101998@gmail.com>
2024-03-05 21:24:04 +0700

    Length of last words

---
+++ length-of-last-words/index.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = function (s) {
+  let result = 0;
+  let i = s.length - 1;
+
+  while (i >= 0) {
+    if (s[i] === ' ' && result === 0) {
+      i--;
+      continue;
+    }
+
+    if (s[i] === ' ' && result > 0) {
+      break
+    }
+
+    result++
+    i--;
+  }
+
+  return result
+};
+
+function main() {
+  console.log(lengthOfLastWord('   fly me   to   the moon  '))
+}
+
+main()
